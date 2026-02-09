e19929468aeffeddce8dc77d728c9d83e160c864
Mohamedyassin5 <mohamedyassin.rezgui@esprit.tn>
2026-02-09 13:45:28 +0100

    user + crop

---
+++ src/main/java/tn/esprit/agri/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package tn.esprit.agri.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tn.esprit.agri.entities.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
